78d277a6d0c952ad92f21f9d25888ee25f0acf79
rubina02akter <rubinaakter0108@gmail.com>
2025-01-24 18:39:19 +0600

    blog design

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -10,11 +10,11 @@ export default async function Profile() {
 
   return (await isAuthenticated()) ? (
     <section className="container mx-auto min-h-screen">
-        <p className="text-3xl font-semibold">hey {user?.family_name} {user?.given_name}👋</p>
+        <p className="text-3xl font-semibold text-center">Welcome {user?.family_name} {user?.given_name}👋</p>
     </section>
   ) : (
-    <div className="container mx-auto min-h-screen text-2xl">
-       This page is protected, please <LoginLink className="font-bold underline">Login</LoginLink> to view it
+    <div className="container mx-auto min-h-screen text-2xl text-center">
+       This page is protected, please <LoginLink className="font-bold text-blue-800 underline">Login</LoginLink> to view it
     </div>
   );
 }
